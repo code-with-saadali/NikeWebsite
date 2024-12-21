94454b21b7453257dd0cafa519c543568973a067
code-with-saadali <saadali786000786@gmail.com>
2024-12-21 16:19:41 +0500

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,43 @@
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+      <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+          <img
+            src={headerLogo}
+            alt='logo'
+            width={129}
+            height={29}
+            className='m-0 w-[129px] h-[29px] hover:scale-110 duration-500'
+          />
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className='font-montserrat leading-normal text-lg text-slate-gray hover:text-black duration-300'
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/' className="hover:scale-105 duration-300 hover:text-gray-500 hover:underline">Sign in</a>
+          <span>/</span>
+          <a href='/' className="hover:scale-105 duration-300 hover:text-gray-500 hover:underline">Explore now</a>
+        </div>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
